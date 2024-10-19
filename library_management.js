fb801855c0dde33a77d3bba169f7043fc80033f1
Rashi Ratan <rashiiratan@gmail.com>
2024-10-19 19:57:06 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -39,3 +39,34 @@ class Section {
         ) //list all books and their availability
     }
 }
+
+//task 3
+class Patron {
+    constructor(name, borrowedBooks) {
+        this.name = name;
+        this.borrowedBooks = []; //create borrowed books array
+    }
+    borrowBook(book) {
+        if (book.isAvailable === true){ //check for book's availability
+        this.borrowedBooks.push(book); //add to patron's borrowed book array
+        book.isAvailable = false; //change book status
+        return `Patron ${this.name} borrowed the book ${book.title} successfully.`;}
+        else {
+        return `Sorry, this book is unavailable`;
+        //display error message
+        }
+    }
+    returnBook(book) {
+        let foundBook = this.borrowedBooks.find(item => item.title === book.title);
+        if (foundBook) { //check if book was borrowed
+            book.isAvailable = true; //change availability
+           this.borrowedBooks = this.borrowedBooks.filter(item => item.title !== foundBook.title);
+           //remove the book from borrowed books
+           return `The book ${book.title} has been returned successfully.`;
+        }
+        else {
+            return `Sorry, this book was not borrowed.`;
+            //dispay error message
+        }
+    }
+}
